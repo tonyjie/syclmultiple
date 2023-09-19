7ef11306ab1c68d4ca6d50f09afea6a3624ad475
Yifan Li <yfli@dhcp-vl2041-40314.eduroam.cornell.edu>
2023-09-19 19:35:59 -0400

    feat: using dual device for picture blurring

--- edge.cpp
+++ edge.cpp
@@ -39,7 +39,8 @@ this code doesn't check for limits (bad, bad, bad!)
 
 #include "image_conv.h"
 
-inline constexpr int filterWidth = 11;
+inline constexpr int filterWidth = 44;
+// inline constexpr int filterWidth = 88;
 inline constexpr int halo = filterWidth / 2;
 
 int main(int argc, char* argv[]) {
@@ -158,10 +159,54 @@ int main(int argc, char* argv[]) {
     std::cout << "\n";
 #endif
 
+// ========== Q1 setting prepare ==========
+  auto inImgWidth = inImage.width();
+
+  /* Using Two Devices to run the Task*/
+  auto inImgHeight_tot = inImage.height();
+  auto inImgHeight_a = inImgHeight_tot / 2; // split the image in half
+  inImgHeight_a = inImgHeight_a / 32 * 32;  // make it a multiple of 32
+  auto inImgHeight_b = inImgHeight_tot - inImgHeight_a;
+
+  auto channels = inImage.channels();
+  auto filterWidth = filter.width();
+  auto halo = filter.half_width();
+
+  auto globalRange_a = sycl::range(inImgWidth, inImgHeight_a);
+  auto globalRange_b = sycl::range(inImgWidth, inImgHeight_b);
+  auto localRange = sycl::range(1, 8);    // It seems Intel have 8 "threads" per "warp"
+
+  auto ndRange_a = sycl::nd_range(globalRange_a, localRange);
+  auto ndRange_b = sycl::nd_range(globalRange_b, localRange);
+
+  auto inBufRange_a =
+      sycl::range(inImgHeight_a + (halo * 2), inImgWidth + (halo * 2)) *
+      sycl::range(1, channels);
+  auto inBufRange_b =
+      sycl::range(inImgHeight_b + (halo * 2), inImgWidth + (halo * 2)) *
+      sycl::range(1, channels);
+
+  auto outBufRange_a =
+      sycl::range(inImgHeight_a, inImgWidth) * sycl::range(1, channels);
+  auto outBufRange_b =
+      sycl::range(inImgHeight_b, inImgWidth) * sycl::range(1, channels);
+
+  auto filterRange = filterWidth * sycl::range(1, channels);
+
+
 #ifdef MYDEBUGS
-    auto t1 = std::chrono::steady_clock::now();  // Start timing
+  std::cout << "inImgWidth: " << inImgWidth << "\ninImgHeight: " << inImgHeight_tot
+            << "\ninImgHeight_a: " << inImgHeight_a << "\ninImgHeight_b: " << inImgHeight_b
+            << "\nchannels: " << channels << "\nfilterWidth: " << filterWidth
+            << "\nhalo: " << halo << "\n";
 #endif
 
+
+#ifdef MYDEBUGS
+    auto t2_start = std::chrono::steady_clock::now();  // Start timing
+#endif
+
+// ======== Q2 submit begin ========== 
 #ifdef DOUBLETROUBLE
     std::array<int, 200> d4;
     // inspired and based upon:
@@ -205,30 +250,46 @@ int main(int argc, char* argv[]) {
       });
     });
 #endif
+// ======== Q2 submit end ==========
 
-  auto inImgWidth = inImage.width();
-  auto inImgHeight = inImage.height();
-  auto channels = inImage.channels();
-  auto filterWidth = filter.width();
-  auto halo = filter.half_width();
+auto t1_kernel_launch_end = std::chrono::steady_clock::now();  // Start timing
 
-  auto globalRange = sycl::range(inImgWidth, inImgHeight);
-  auto localRange = sycl::range(1, 32);
-  auto ndRange = sycl::nd_range(globalRange, localRange);
+// ======== Q2 measurement ==========
+#ifdef DOUBLETROUBLE
+    // e2.wait(); // make sure all digits are done being computed
+    myQueue2.wait();
+    // t2 counter
+    auto t2_end = std::chrono::steady_clock::now();  // Stop timing
+    
+    sycl::host_accessor myD4(outD4); // the scope of the buffer continues - so we must not use d4[] directly
+    // std::cout << "First 800 digits of pi: ";
+    // for (int i = 0; i < 200; ++i) printf("%.4d", myD4[i]);
+    // std::cout << "\n";
 
-  auto inBufRange =
-      sycl::range(inImgHeight + (halo * 2), inImgWidth + (halo * 2)) *
-      sycl::range(1, channels);
-  auto outBufRange =
-      sycl::range(inImgHeight, inImgWidth) * sycl::range(1, channels);
+    double time2A = (e2.template get_profiling_info<
+                         sycl::info::event_profiling::command_end>() -
+                     e2.template get_profiling_info<
+                         sycl::info::event_profiling::command_start>());
 
-  auto filterRange = filterWidth * sycl::range(1, channels);
+    double time2B = 
+        (std::chrono::duration_cast<std::chrono::microseconds>(t2_end - t2_start)
+             .count());                     
 
-#ifdef MYDEBUGS
-  std::cout << "inImgWidth: " << inImgWidth << "\ninImgHeight: " << inImgHeight
-            << "\nchannels: " << channels << "\nfilterWidth: " << filterWidth
-            << "\nhalo: " << halo << "\n";
+    // double time2C = 
+    //     (std::chrono::duration_cast<std::chrono::microseconds>(t2_end - t1_kernel_launch_end)
+    //          .count());
+
+    std::cout << "====== Pi Calculation ( Queue 2 ) =======\n";
+
+    std::cout << "profiling: Operation completed on device2 in " << time2A
+              << " nanoseconds (" << time2A / 1.0e9 << " seconds)\n";
+    std::cout << "chrono: Operation completed on device2 in " << time2B * 1000
+              << " nanoseconds (" << time2B * 1000 / 1.0e9 << " seconds)\n";
+    // std::cout << "chrono [end - kernel_launch_end]: Operation completed on device2 in " << time2C
+    //           << " nanoseconds (" << time2C / 1.0e9 << " seconds)\n";
 #endif
+// ========= Q2 measurement end ==========
+
 
   // Always good to limit scope of accessors,
   // so a good SYCL program will introduce a scope before
@@ -237,18 +298,30 @@ int main(int argc, char* argv[]) {
   // to should ONLY be accessed with an accessor. That
   // goes for the host just as much as the device.
 
+
+
   {
-    auto inBuf = sycl::buffer{inImage.data(), inBufRange};
-    auto outBuf = sycl::buffer<float, 2>{outBufRange};
+    // ======== Picture blurring submit begin ==========
+    auto inBuf_a = sycl::buffer{inImage.data(), inBufRange_a};
+    auto outBuf_a = sycl::buffer<float, 2>{outBufRange_a};
+    outBuf_a.set_final_data(outImage.data());
+
+    auto inBuf_b = sycl::buffer{inImage.data() + inImgHeight_a * channels * (inImgWidth + halo * 2), inBufRange_b};
+    auto outBuf_b = sycl::buffer<float, 2>{outBufRange_b};
+    outBuf_b.set_final_data(outImage.data() + inImgHeight_a * channels * inImgWidth);
+
     auto filterBuf = sycl::buffer{filter.data(), filterRange};
-    outBuf.set_final_data(outImage.data());
+
+#ifdef MYDEBUGS
+    auto t1_start = std::chrono::steady_clock::now();  // Start timing
+#endif
 
     sycl::event e1 = myQueue1.submit([&](sycl::handler& cgh1) {
-      sycl::accessor inAccessor{inBuf, cgh1, sycl::read_only};
-      sycl::accessor outAccessor{outBuf, cgh1, sycl::write_only};
+      sycl::accessor inAccessor{inBuf_a, cgh1, sycl::read_only};
+      sycl::accessor outAccessor{outBuf_a, cgh1, sycl::write_only};
       sycl::accessor filterAccessor{filterBuf, cgh1, sycl::read_only};
 
-      cgh1.parallel_for(ndRange, [=](sycl::nd_item<2> item) {
+      cgh1.parallel_for(ndRange_a, [=](sycl::nd_item<2> item) {
         auto globalId = item.get_global_id();
         globalId = sycl::id{globalId[1], globalId[0]};
 
@@ -285,7 +358,58 @@ int main(int argc, char* argv[]) {
       });
     });
 
-    myQueue1.wait_and_throw();
+
+    sycl::event e3 = myQueue2.submit([&](sycl::handler& cgh1) {
+      sycl::accessor inAccessor{inBuf_b, cgh1, sycl::read_only};
+      sycl::accessor outAccessor{outBuf_b, cgh1, sycl::write_only};
+      sycl::accessor filterAccessor{filterBuf, cgh1, sycl::read_only};
+
+      cgh1.parallel_for(ndRange_b, [=](sycl::nd_item<2> item) {
+        auto globalId = item.get_global_id();
+        globalId = sycl::id{globalId[1], globalId[0]};
+
+        auto channelsStride = sycl::range(1, channels);
+        auto haloOffset = sycl::id(halo, halo);
+        auto src = (globalId + haloOffset) * channelsStride;
+        auto dest = globalId * channelsStride;
+
+        // 100 is a hack - so the dim is not dynamic
+        float sum[100];
+        assert(channels < 100);
+
+        for (size_t i = 0; i < channels; ++i) {
+          sum[i] = 0.0f;
+        }
+
+        for (int r = 0; r < filterWidth; ++r) {
+          for (int c = 0; c < filterWidth; ++c) {
+            auto srcOffset =
+                sycl::id(src[0] + (r - halo), src[1] + ((c - halo) * channels));
+            auto filterOffset = sycl::id(r, c * channels);
+
+            for (int i = 0; i < channels; ++i) {
+              auto channelOffset = sycl::id(0, i);
+              sum[i] += inAccessor[srcOffset + channelOffset] *
+                        filterAccessor[filterOffset + channelOffset];
+            }
+          }
+        }
+
+        for (size_t i = 0; i < channels; ++i) {
+          outAccessor[dest + sycl::id{0, i}] = sum[i];
+        }
+      });
+    });
+
+    // ======== Q1 submit end ==========
+
+// #ifdef MYDEBUGS
+//     auto t1_kernel_end = std::chrono::steady_clock::now();  // Start timing
+// #endif
+
+    // myQueue1.wait_and_throw();
+    myQueue1.wait();
+    myQueue2.wait();
 
 #ifdef MYDEBUGS
     // Timing code is from our book (2nd edition) -
@@ -294,37 +418,45 @@ int main(int argc, char* argv[]) {
     // Check https://tinyurl.com/reinders-4class for link
     // to copy of 2nd edition ("Learn SYCL").
 
+// ======= Q1 measurement ==========
+    auto t1_end = std::chrono::steady_clock::now();  // Stop timing
+
     double time1A = (e1.template get_profiling_info<
                          sycl::info::event_profiling::command_end>() -
                      e1.template get_profiling_info<
                          sycl::info::event_profiling::command_start>());
-    auto t2 = std::chrono::steady_clock::now();  // Stop timing
-    double time1B =
-        (std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+    double time1B = (e3.template get_profiling_info<
+                         sycl::info::event_profiling::command_end>() -
+                     e3.template get_profiling_info<
+                         sycl::info::event_profiling::command_start>());
+    double time1C =
+        (std::chrono::duration_cast<std::chrono::microseconds>(t1_end - t1_start)
              .count());
+    // double time1C =
+    //     (std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1_kernel_begin)
+    //          .count());
+    // double time1D = 
+    //     (std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1_kernel_end)
+    //          .count());
 
+    std::cout << "====== Picture Blurring (Queue 1 & 2) =======\n";
     std::cout << "profiling: Operation completed on device1 in " << time1A
               << " nanoseconds (" << time1A / 1.0e9 << " seconds)\n";
-    std::cout << "chrono: Operationd completed on device1 in " << time1B * 1000
-              << " nanoseconds (" << time1B * 1000 / 1.0e9 << " seconds)\n";
-    std::cout << "chrono more than profiling by " << (time1B * 1000 - time1A)
-              << " nanoseconds (" << (time1B * 1000 - time1A) / 1.0e9
-              << " seconds)\n";
+    std::cout << "profiling: Operation completed on device2 in " << time1B
+              << " nanoseconds (" << time1B / 1.0e9 << " seconds)\n";
+    std::cout << "chrono: Operation completed on device1 and device 2 in " << time1C * 1000
+              << " nanoseconds (" << time1C * 1000 / 1.0e9 << " seconds)\n";
+    // std::cout << "chrono [end - kernel_launch_begin]: Operation completed on device1 in " << time1C
+    //           << " nanoseconds (" << time1C / 1.0e9 << " seconds)\n";
+    // std::cout << "chrono [end - kernel_launch_end]: Operation completed on device1 in " << time1D
+    //           << " nanoseconds (" << time1D / 1.0e9 << " seconds)\n";
 
-#ifdef DOUBLETROUBLE
-    e2.wait(); // make sure all digits are done being computed
-    sycl::host_accessor myD4(outD4); // the scope of the buffer continues - so we must not use d4[] directly
-    std::cout << "First 800 digits of pi: ";
-    for (int i = 0; i < 200; ++i) printf("%.4d", myD4[i]);
-    std::cout << "\n";
 
-    double time2A = (e2.template get_profiling_info<
-                         sycl::info::event_profiling::command_end>() -
-                     e2.template get_profiling_info<
-                         sycl::info::event_profiling::command_start>());
-    std::cout << "profiling: Operation completed on device2 in " << time2A
-              << " nanoseconds (" << time2A / 1.0e9 << " seconds)\n";
-#endif
+    // std::cout << "chrono more than profiling by " << (time1B * 1000 - time1A)
+    //           << " nanoseconds (" << (time1B * 1000 - time1A) / 1.0e9
+    //           << " seconds)\n";
+
+
 #endif
   }
 }
